035709ffd1f2f27906947c39cb8a04b0b5227556
A.Rahman Mufarrah <a.mufarrah@gmail.com>
2023-01-08 16:43:25 +0300

    first commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+
+function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div >
+      <form className="create-note">
+        {isExpanded && <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />}
+        <textarea onClick={() => {
+          setIsExpanded(true);
+        }}
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? "3" : "1"}
+        />
+        <Zoom in={isExpanded ? true : false}>
+          <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
+
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HighlightIcon from '@mui/icons-material/Highlight';
+
+function Header() {
+  return (
+    <header>
+      <h1><HighlightIcon /> Take a Note</h1>
+    </header>
+  );
+}
+
+export default Header;
